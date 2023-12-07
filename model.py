e805f304600ee30d91b645c3924e81d73b71d478
Noviani <67784080+noviani04@users.noreply.github.com>
2023-12-07 11:10:26 +0800

    Upload File

---
+++ model.py
@@ -0,0 +1,149 @@
+import keras
+latent_dim = 256
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 5000  # Number of samples to train on.
+# Path to the data txt file on disk.
+data_path = 'datasetversikata.txt'
+with open(data_path, encoding='utf-8') as f:
+    lines = f.readlines()
+lines[5]
+
+
+# Vectorize the data.
+input_texts = []
+target_texts = []
+input_characters = set()
+target_characters = set()
+with open(data_path, "r", encoding="utf-8") as f:
+    lines = f.read().split("\n")
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    input_text, target_text, = line.split("\t")
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = "\t" + target_text + "\n"
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+    for char in input_text:
+        if char not in input_characters:
+            input_characters.add(char)
+    for char in target_text:
+        if char not in target_characters:
+            target_characters.add(char)
+
+input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters)
+num_decoder_tokens = len(target_characters)
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
+target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+
+#Build the model
+# Define an input sequence and process it.
+encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
+encoder = keras.layers.LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
+
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_dense = keras.layers.Dense(num_decoder_tokens, activation="softmax")
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Restore the model
+model = keras.models.load_model('21nov2.h5', compile=False)
+
+# Define sampling models
+# Restore the model and construct the encoder and decoder.
+
+encoder_inputs = model.input[0]  # input_1
+encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = keras.Model(encoder_inputs, encoder_states)
+
+decoder_inputs = model.input[1]  # input_2
+decoder_state_input_h = keras.Input(shape=(latent_dim,))
+decoder_state_input_c = keras.Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_lstm = model.layers[3]
+decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs
+)
+decoder_states = [state_h_dec, state_c_dec]
+decoder_dense = model.layers[4]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = keras.Model(
+    [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+)
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
+reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq, verbose=0)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index["\t"]] = 1.0
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ""
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if sampled_char == "\n" or len(decoded_sentence) > max_decoder_seq_length:
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.0
+
+        # Update states
+        states_value = [h, c]
+    return decoded_sentence
+
+def predict_output(input_text):
+    words = input_text.split(' ')
+    transliterated_words = []
+    for word in words:
+        input_seq = np.zeros((1, max_encoder_seq_length, num_encoder_tokens), dtype='float32')
+        for t, char in enumerate(word):
+            input_seq[0, t, input_token_index[char]] = 1.0
+        input_seq[0, t + 1:, input_token_index[" "]] = 1.0
+        decoded_word = decode_sequence(input_seq)
+        transliterated_words.append(decoded_word.rstrip('\n'))
+    return ' '.join(transliterated_words)
