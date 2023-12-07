2ea9c17d40372543aa4735c06ea3f66c83bcc604
Noviani <67784080+noviani04@users.noreply.github.com>
2023-12-07 12:17:33 +0800

    Update arablatin.py

--- arablatin.py
+++ arablatin.py
@@ -4,7 +4,7 @@ import io
 from keras.utils import load_img
 import model
 
-pytesseract.pytesseract.tesseract_cmd = None
+#pytesseract.pytesseract.tesseract_cmd = None
 
 import cv2
 #from pytesseract import Output
